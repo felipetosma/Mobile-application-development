7333bdb514a431322f414af90e042d250b84f32c
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 21:19:06 -0300

    atualização dos nomes do arquivos

--- src/Desafio2.kt
+++
@@ -1,9 +0,0 @@
-fun main (){
-    var nota1: Double= 5.0
-    var nota2: Double= 10.0
-    var nota3: Double= 8.0
-
-    var media = (nota1 + nota2 + nota3)/3
-
-    println(String.format("%.2f",media))
-}
