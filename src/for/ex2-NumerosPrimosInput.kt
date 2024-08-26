76c76297e167a669c3e933ccad59b0788a084213
Felipe Amador <143852525+felipetosma@users.noreply.github.com>
2024-08-26 20:17:26 -0300

    consertando o codigo numerosPrimos

--- src/for/ex2-NumerosPrimosInput.kt
+++ src/for/ex2-NumerosPrimosInput.kt
@@ -1,21 +1,7 @@
 fun main (){
     
     println("Digite um numero: ")
-    val number = readLine() // Lê a entrada do usuário como uma String
-
-    if(number == null){
-        println("Entrada inválida.")
-        return // Sai da função main se a entrada for nula
-    }
-
-    //Tenta converter a entrada de idade para um valor numérico
-    val num = number.toIntOrNull()
-
-    //Verifica se a conversão foi bem sucedida ou se a entrada é invalida
-    if(num == null){
-        println("Numero inválido. Por favor, insira um número válido.")
-        return // Sai da função main se a conversão não for bem sucedida.
-    }
+    val num = readLine()!!.toInt() // Lê a entrada do usuário como uma String
     var isPrime = true
     
     for(i in 2 until num) {
